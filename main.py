76a102cc9bb1e52d6ed5648f606aeafff2b4c8b1
wildbirdStan <stan.johler@wildbirdtrust.org>
2026-04-06 14:37:54 +0200

    OpenAI Chatmodel + MCP Server + chat_agent

--- main.py
+++ main.py
@@ -1,12 +1,60 @@
-# /// script
-# requires-python = ">=3.13"
-# dependencies = []
-# ///
+import asyncio
+from langchain_core.tools.base import BaseTool
+from langchain_openai import ChatOpenAI
+from langchain.messages import HumanMessage, AIMessage
+from langchain.agents import create_agent
+from langchain_mcp_adapters.client import MultiServerMCPClient
+import os
+from dotenv import load_dotenv
 
 
-def main() -> None:
-    print("Hello from main.py!")
+
+
+async def main() -> None:
+    load_dotenv()
+
+    
+    openai_api_key: str = os.getenv("OPENAI_API_KEY")
+    rapid_api_key_football = os.getenv("RAPID_API_KEY_FOOTBALL")
+
+    if not openai_api_key or not rapid_api_key_football:
+        raise ValueError()
+
+    client: MultiServerMCPClient = MultiServerMCPClient({
+        "soccer_server": {
+            "transport": "stdio",
+            "command": "python",
+            "args": [
+                "./soccer-mcp-server/soccer_server.py"
+            ],
+            "env": {
+                "RAPID_API_KEY_FOOTBALL": rapid_api_key_football
+            }
+        }
+    })
+    
+    tools: list[BaseTool] = await client.get_tools()
+
+
+
+    llm: ChatOpenAI = ChatOpenAI(
+        model="gpt-5.4",
+        api_key=openai_api_key
+    )
+
+    llm_agent = create_agent(
+        model=llm,
+        tools=tools,
+        system_prompt="You are a code agent and you are providing informations about football (soccer) based on the soccer_server functions. Evaluate based on the soccer_server tools provided"
+    )
+
+
+
+    answer = await llm_agent.ainvoke({
+        "messages": [HumanMessage(content="What are the upcoming champions league fixtures and if the soccer_server returns 403 forbidden tell me the error why it is not working")]
+    })
+    print(answer["messages"][-1].content)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
